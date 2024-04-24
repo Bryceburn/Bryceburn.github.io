84307a1e92488570a22f515ff5d09738d662e888
Bryceburn <144712922+Bryceburn@users.noreply.github.com>
2024-04-24 16:57:23 +0000

    final changes

---
+++ scripts/main.js
@@ -0,0 +1,31 @@
+console.log("task 1")
+for (let i=i; i<10; i++){
+    console.log(i*i);
+}
+
+console.log("task 2")
+for (let i = 5; i >= 1; i--) {
+    console.log(i);
+}
+console.log("Blastoff!");
+
+console.log("task 3")
+for (let i = 1; i <= 50; i++) 
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+
+console.log("task 4")
+let sum = 0;
+for (let i = 1; i <= 100; i++) {
+    sum += i;
+}
+console.log(sum);
+
+console.log("task 5")
+let factorial = 1;
+for (let i = 1; i <= 10; i++) {
+    factorial *= i;
+    console.log(factorial);
+}
+
